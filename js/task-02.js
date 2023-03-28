dbd0c1c67fd434fdfc64f538f14909c09795d18b
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-03-28 17:04:12 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+const allIngredients = [];
+
+for (let i=0; i < ingredients.length; i += 1 ){
+  const li = document.createElement('li');
+  li.textContent = ingredients[i];
+  li.classList.add('item');
+
+  allIngredients.push(li);
+};
+// console.dir(allIngredients)
+listEl.append(...allIngredients);
