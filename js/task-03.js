4d891335231a0338c8ab63f7803fc5793596a59d
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-03-28 19:20:41 +0300

    fin

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector('.gallery');
+
+const markup = images.map(({ url, alt }) => 
+  `<li class="gallery-item"><img src="${url}" alt="${alt}"></li>`
+  ).join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
