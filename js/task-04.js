5eabe267d8905b6c4d988ba3a7e2df0a4f68b725
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-03-29 14:31:55 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,27 @@
+const counter = {
+  counterValue: 0,
+  increment(){
+    this.counterValue += 1;
+  },
+
+  decrement(){
+    this.counterValue -= 1;
+  },
+};
+
 const valueEl = document.querySelector('#value');
-const buttonEl = document.querySelector('button');
-const counterValue = 0;
+const plusBtn = document.querySelector('[data-action="increment"]');
+const minusBtn = document.querySelector('[data-action="decrement"]');
+
+
+plusBtn.addEventListener("click",function(){
+  counter.increment();
+  console.log(counter)
+  valueEl.textContent = counter.counterValue;
+});
+
+minusBtn.addEventListener("click",function(){
+  counter.decrement();
+  console.log(counter)
+  valueEl.textContent = counter.counterValue;
+});
