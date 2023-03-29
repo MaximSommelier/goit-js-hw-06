bb735732e900a52695fdcbd7ed365560da38d7e8
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-03-30 01:15:07 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+const span = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
+button.addEventListener('click', changeBodyColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+ function changeBodyColor(event){
+  button.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+};
