a413e3e8bafc884875746ea09d6dd2e1700b4075
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-03-30 00:30:50 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
+// const email = document.querySelector('email');
+// const password = document.querySelector('password');
 
+form.addEventListener('submit', onSubmit);
+const alert = `Усі поля мають бути заповені!`
+
+function onSubmit(event){
+    event.preventDefault();
+    
+    const {email, password} = event.currentTarget.elements;
+    const dataUser ={
+        email: email.value,
+        password: password.value,    
+    }
+    console.log(dataUser)
+    if (email || password  === ""){
+        console.log(alert)
+    }
+    document.onSubmit('form').reset();
+};
